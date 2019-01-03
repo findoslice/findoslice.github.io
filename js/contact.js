921a2a4b7173b48f321538ee894098c16b898f95
Findlay Smith <findlayksmith@gmail.com>
2019-01-03 05:14:19 +0000

    very professional™ contact page@

---
+++ js/contact.js
@@ -0,0 +1,46 @@
+'use strict';
+
+const Icon = (props) => {
+    return(
+        <a href = {props.link} className = "contacticon">
+            <div className = {props.fa}></div>
+        </a>
+    )
+}
+
+class ContactLinks extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+                    "links": {
+                        "github": {
+                            "link": "https://github.com/findoslice",
+                            "fa": "fab fa-github"
+                        },
+                        "linkedin": {
+                            "link": "https://www.linkedin.com/in/findlay-smith-a524a8151/",
+                            "fa": "fab fa-linkedin"
+                        },
+                        "mysterioso": {
+                            "link": "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+                            "fa": "fas fa-question"
+                        }
+                    }
+                 }
+  }
+
+  render() {
+    return (
+      <div>
+        {Object.keys(this.state.links).map((key, i) => {
+            return(
+                <Icon link = {this.state.links[key].link} fa = {this.state.links[key].fa} key = {i}></Icon>
+            );
+        })}
+      </div>
+    );
+  }
+}
+
+const domContainer = document.querySelector('#contactlinks');
+ReactDOM.render(<ContactLinks />, domContainer);
